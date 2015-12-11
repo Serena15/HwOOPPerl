83833d5ee8999a1ab32b069f8cc0c95fa159c2b6
Serena15 <raketa9390@mail.ru>
2015-12-12 00:36:21 +0300

    Update File.pm

--- File.pm
+++ File.pm
@@ -1,6 +1,7 @@
 package Local::Iterator::File;
 use strict;
 use warnings;
+use DDP;
 
 sub new {
 	my ($class, %params) = @_;
@@ -16,48 +17,6 @@ sub next {
 	my $fh = $self->{fh};
 	my $row = <$fh>;			
 	unless(defined $row) {
-		return (undef, 1);	
-	}	
-	chomp $row;	
-	return ($row, 0);
-}
-
-sub all {
-	my ($self) = @_;	
-	my @array = ();
-	my $fh = $self->{fh};
-	while(<$fh>) {
-		chomp;
-		push @array, $_;	
-	}
-	return \@array;
-}
-
-package Local::Iterator::File;
-
-use strict;
-use warnings;
-use DDP;
-
-sub new {
-	my ($class, %params) = @_;
-	if(defined $params{filename}) {
-		open(my $fh, '<', $params{filename});
-		$params{fh} = $fh;
-	}
-	return bless \%params, $class;
-}
-
-sub next {
-	my ($self) = @_;
-	my $fh = $self->{fh};
-	#print "------\n";
-	my $row = <$fh>;
-	#print "------\n";	
-	#p $row;
-	#print "------\n";			
-	unless(defined $row) {
-		#close($fh);
 		return (undef, 1);	
 	}	
 	chomp $row;	
